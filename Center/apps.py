6cf18f04d3adc2389d3d981305e241af9b149578
MissNjuguna-blip <vwaruinu@gmail.com>
2026-08-14 12:21:37 +0300

    center

--- Center/apps.py
+++ Center/apps.py
@@ -1,6 +1,6 @@
-from django.apps import AppConfig
+# from django.apps import AppConfig
 
 
-class CenterConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'center'
+# class CenterConfig(AppConfig):
+#     default_auto_field = 'django.db.models.BigAutoField'
+#     name = 'center'
